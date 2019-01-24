fe3160a318bdab095874740f65963147d5bccf11
Terence1991 <cplterencehawes@gmail.com>
2019-01-24 16:45:24 +0000

    api implemented

--- src/apis/youtube.js
+++ src/apis/youtube.js
@@ -1,1 +1,14 @@
-const KEY= AIzaSyCaAIKyfp5gp7gKq2tyaxelkfVILgpTCmQ
+import axios from 'axios'
+
+
+const KEY = 'AIzaSyCaAIKyfp5gp7gKq2tyaxelkfVILgpTCmQ'
+
+export default axios.create({
+  baseURL: 'https://www.googleapis.com/youtube/v3',
+  params: {
+    part: 'snippet',
+    maxResults: 5,
+    key: KEY
+
+  }
+})
