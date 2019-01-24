6e2e9d37d3958236688e22cafb1474a95fadaa43
Terence1991 <cplterencehawes@gmail.com>
2019-01-24 02:42:41 +0000

    state initialised

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,13 +1,24 @@
 import React from 'react'
 
 class SearchBar extends React.Component {
+    
+  state = {text: ''}
+
+  onInputChange = () => {
+    
+  }
+
   render() {
     return (
     <div className='search-bar ui segment'>
       <form className='ui from'>
         <div className='field'>
           <label>Video Search</label>
-          <input type='text'></input>
+          <input 
+          type='text' 
+          value={this.state.text}
+          onChange={this.onInputChange}
+          />
           </div>
       </form>
     </div>
