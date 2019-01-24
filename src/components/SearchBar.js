04be3b851522d306937046fbc15ae01f19d86220
Terence1991 <cplterencehawes@gmail.com>
2019-01-24 02:22:47 +0000

    bug fixed

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -6,10 +6,9 @@ class SearchBar extends React.Component {
     <div className='search-bar ui segment'>
       <form className='ui from'>
         <div className='field'>
-          <label>
-          Video Search
-          </label>
-        </div>
+          <label>Video Search</label>
+          <input type='text'></input>
+          </div>
       </form>
     </div>
     );
