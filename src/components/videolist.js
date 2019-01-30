c1969f46935e3c863957ccafc5da430b8f5100b8
Terence1991 <cplterencehawes@gmail.com>
2019-01-30 16:52:25 +0000

    bug fixed

--- src/components/videolist.js
+++ src/components/videolist.js
@@ -4,7 +4,7 @@ import VideoItem from './videoitem.js'
 
 const VideoList = ({videos, onVideoSelect}) => {
   const renderedList = videos.map((video) => {
-   return <VideoItem onVideoSelect={onVideoSelect} video={video}/>
+   return <VideoItem key={video.id.videoId} onVideoSelect={onVideoSelect} video={video}/>
   })
 
    return (
