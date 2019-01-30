4f1ffdaf902d244fa94c046b7375e30221ab8ba4
Terence1991 <cplterencehawes@gmail.com>
2019-01-30 16:08:41 +0000

    player added

--- src/components/VideoDetail.js
+++ src/components/VideoDetail.js
@@ -6,8 +6,14 @@ const VideoDetail = ({video}) => {
   if(!video) {
 return <div>Loading!!!!</div>
   }
+
+  const vidSrc = `https://www.youtube.com/embed/${video.id.videoId}`
+
  return ( 
    <div>
+     <div className='ui-embed'>
+     <iframe title="player" src={vidSrc} />
+     </div>
   <div className= "ui segment">
      <h4 className="header">
      {video.snippet.title}
