bb181032b05233dc5031201868f35acdcafcc7f0
Terence1991 <cplterencehawes@gmail.com>
2019-01-30 17:06:56 +0000

    styling added

--- src/components/App.js
+++ src/components/App.js
@@ -25,8 +25,16 @@ class App extends React.Component {
     return ( 
     <div className='ui container'> 
       <SearchBar onTextSubmit={this.onTextSubmit}/>
+      <div className='ui grid'>
+      <div className='ui row'>
+      <div className="eleven wide column">
       <VideoDetail video={this.state.selectedVideos}/>
+      </div>
+      <div className="five wide column">
       <VideoList onVideoSelect={this.onVideoSelect} videos={this.state.videos}/>
+      </div>
+      </div>
+      </div>
     </div>
     );
   }
